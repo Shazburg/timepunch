594191b983409de51ce25f8d602487979d5ba76c
Josh Matthews <jm@scrug.net>
2010-11-08 16:17:32 -0600

    Mostly working timesheets views

--- admin/controllers/timesheets.rb
+++ admin/controllers/timesheets.rb
@@ -2,6 +2,9 @@ Admin.controllers :timesheets do
 
   get :index do
     @employees = Employee.all(:enabled => true, :order => :last_name)
+    @start_date = params[:start_date] || (Date.today.beginning_of_week - 7).to_s
+    @end_date = params[:end_date] || (Date.today.beginning_of_week - 1).to_s
+    @date_range = @start_date..@end_date
     render 'timesheets/index'
   end
 
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,6 +6,19 @@ class Employee
     "#{last_name}, #{first_name}"
   end
 
+  def total_hours(start_date, end_date)
+    results = self.punches.all(:fields => [:in, :timestamp], :timestamp => (start_date..end_date))
+    results.each do |result|
+      case result
+      when result.in = 0
+        @time_out = time_out + result.timestamp
+      when result.in = 1
+        @time_in = time_in + result.timestamp
+      end
+    end
+    return @time_out
+  end
+
   # property <name>, <type>
   property :id, Serial
   property :last_name, String
